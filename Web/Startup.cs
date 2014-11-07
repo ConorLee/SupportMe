6260cab86dbabcac1fe889603e97073f1485b3e7
Andrew Fletcher <andrew@af-web.co.uk>
2014-11-07 16:14:53 +0000

    Initial Project

---
+++ Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SupportMe.Startup))]
+namespace SupportMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
